758d9e0160fd193018bd9c140dc14f4554eea980
jazeelpe <jazeelpe@gmail.com>
2024-02-05 22:37:19 +0530

    third commit - basic redux store

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Home from './src/components/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
+const stack = createNativeStackNavigator();
 
+const AppNavigation = () => {
+  <NavigationContainer>
+    <stack.Navigator>
+      <stack.Screen name="Home" component={Home} />
+    </stack.Navigator>
+  </NavigationContainer>;
+};
 const App = () => {
-  return <Home />;
+  return (
+    <Provider store={store}>
+      <AppNavigation />;
+    </Provider>
+  );
 };
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
-import { productData } from '../utils/productData';
-
+import {productData} from '../utils/productData';
 
 const Home = () => {
   return (
@@ -11,7 +17,51 @@ const Home = () => {
         data={productData}
         contentContainerStyle={style.contentcontainer}
         renderItem={({item, index}) => {
-          return <View style={style.flatcontainer}></View>;
+          return (
+            <View style={style.flatcontainer}>
+              <Image
+                source={{uri: item.image}}
+                style={{width: 140, height: 140, resizeMode: 'contain'}}
+              />
+              <View style={{width: 240, height: '100%'}}>
+                <View>
+                  <Text
+                    style={{color: '#000', fontSize: 18, fontWeight: '500'}}
+                    numberOfLines={1}>
+                    {item.name}
+                  </Text>
+                  <Text style={{color: '#000', marginVertical: 5}}>
+                    {item.brand}
+                  </Text>
+                  <Text style={{color: 'green'}}>₹{item.price}</Text>
+                </View>
+
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 5,
+                    marginTop: 8,
+                  }}>
+                  {item.qty !== 0 ? (
+                    <>
+                      <TouchableOpacity style={style.btn}>
+                        <Text style={{textAlign: 'center'}}>-</Text>
+                      </TouchableOpacity>
+                      <Text style={{color: '#000'}}>{item.qty}</Text>
+                      <TouchableOpacity style={style.btn}>
+                        <Text style={{textAlign: 'center'}}>+</Text>
+                      </TouchableOpacity>
+                    </>
+                  ) : (
+                    <TouchableOpacity style={style.btn}>
+                      <Text style={{textAlign: 'center'}}>Add to cart</Text>
+                    </TouchableOpacity>
+                  )}
+                </View>
+              </View>
+            </View>
+          );
         }}
       />
     </View>
@@ -35,11 +85,20 @@ const style = StyleSheet.create({
   flatcontainer: {
     width: '100%',
     height: 150,
-    backgroundColor:"#fff",
-    borderRadius:8,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 5,
+    flexDirection: 'row',
+    borderWidth: 1,
   },
   contentcontainer: {
     padding: 10,
-    gap:10,
+    gap: 10,
+  },
+  btn: {
+    backgroundColor: 'green',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderRadius: 10,
   },
 });
